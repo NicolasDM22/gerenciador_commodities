0e4f6c000e440df8ea0ea029e7f656dee29e6fae
Otávio <161334419+Otavio66@users.noreply.github.com>
2025-11-25 20:53:10 -0300

    Drop commodity-related tables from MySQL
    Remove existing tables related to commodities from the database.

---
+++ gerenciador/app_db/mysql0015.sql
@@ -0,0 +1,6 @@
+
+DROP TABLE IF EXISTS `commodity_descriptive_metrics`;
+DROP TABLE IF EXISTS `commodity_national_forecasts`;
+DROP TABLE IF EXISTS `commodity_regional_comparisons`;
+DROP TABLE IF EXISTS `commodity_prices`;
+DROP TABLE IF EXISTS `commodities`;
